71938ae2546ece5ba63b5f3884a44cd6081eb2ee
Uriel <60166735+peanocurve@users.noreply.github.com>
2020-05-21 11:13:03 -0500

    más cosas

--- ea2project.R
+++ ea2project.R
@@ -1,19 +1,19 @@
 rm(list = ls())
 library(lattice)
 library(ggplot2)
-library(nutshell)
 library(tidyverse)
 library(fastDummies)
 library(corrplot)
+library(readxl)
 
 #Lectura y limpieza de datos
 read_nba <- function(path){
   path %>% 
-    read_xlsx() %>% 
+    read_excel() %>% 
     mutate(Year = str_extract_all(path, 'Documents/GitHub/EA2project/NBASEASONSUMMARY//NBA|\\d+-\\d+|.xlsx')[[1]][2])
 }
 
-divconf<-read_xlsx("Documents/GitHub/EA2project/conference-division.xlsx")
+divconf<-read_excel("Documents/GitHub/EA2project/conference-division.xlsx")
 
 nba_raw <- dir("Documents/GitHub/EA2project/NBASEASONSUMMARY/",full.names = T) %>%
   map_df(read_nba) 
@@ -25,19 +25,35 @@ nba <- nba_raw %>%
 
 
 ##Análisis exploratorio
+#Cantidad de triples lanzados promedio de las últimas 10 temporadas y de puntos
+mediatriples<-aggregate(nba[,12], list(nba$Year), mean)
+ggplot(data=mediatriples, aes(x=mediatriples$Group.1, y=mediatriples$`3PA`)) + geom_bar(stat="identity", width=0.3) +ggtitle('Triples intentados promedio por temporada')+labs(x='Temporada',y="Triples intentados por partido")
+mediapuntos<-aggregate(nba[,7], list(nba$Year), mean)
+ggplot(data=mediapuntos, aes(x=mediapuntos$Group.1, y=mediapuntos$PTS)) + geom_bar(stat="identity", width=0.3) +ggtitle('Puntos promedio por partido cada temporada')+labs(x='Temporada',y="Puntos por partido promedio")
+ggplot(nba,aes(x=nba$`3PA`, y=nba$PTS)) +geom_point()
+ptsreg<-lm(PTS~`3PA`, data=nba)
+summary(ptsreg)
+plot(ptsreg)
+
+#regresion plus/minus vs win%
+
+rwplus<-lm(`WIN%`~`plus/minus`,data=nba)
+ggplot(nba,aes(y=nba$`WIN%`, x=nba$`plus/minus`)) +geom_point()+labs(y='Porcentaje de juegos ganados', x='plus/minus')
+summary(rwplus)
 
 #Veamos que variables son reelevantes 
 
 exploranba <- nba_raw %>% 
   select_if(is.numeric) %>% 
-  select(-c(GP,W,L,MIN,`plus/minus`))
+  select(-c(GP,W,L,MIN,`plus/minus`,OREB,DREB))
   
 pairs(exploranba)
 summary(exploranba)
 
 exploranba <- nba_raw %>% 
   select_if(is.numeric) %>% 
-  select(-c(GP,W,L,MIN,`plus/minus`,FGM,FGA,`3PA`,`OREB`,`DREB`,`BLK`,`BLKA`,`FTM`,PFD,PF,AST))
+  select(-c(GP,W,L,MIN,`plus/minus`,FGM,FGA,`3PM`,`OREB`,`DREB`,`BLK`,`BLKA`,`FTM`,PFD,PF,AST))
+pairs(exploranba)
 
 #Varianza de variebles de interés
 apply(exploranba,2,sd)
@@ -62,17 +78,26 @@ ggplot(nba, aes(x=`WIN%`))+ geom_histogram(color="darkblue", fill="lightblue", b
 
 stars(exploranba,key.loc=c(-0.7,17))
 
+#Resumen exlploranba
+summary(exploranba)
+
 #Indicador, suponiendo el modelo, que los errores son normales
 qqnorm(exploranba$`WIN%`)
 
-#Regresión e inferencia
-wreg<-lm(`WIN%`~ (`FG%`+PTS+`3PM` +`3P%` + FTA + `FT%` + REB + TOV + STL), data=nba)
+#Regresión e inferencia considerando la conferncia
+wregconf<-lm(`WIN%`~ (`FG%`+PTS+`3PA` +`3P%` + FTA + `FT%` + REB + TOV + STL+ conference_Eastern), data=nba)
+summary(wregconf)
+
+#Regresión e inferencia sin considerar la conferencia
+wreg<-lm(`WIN%`~ (`FG%`+PTS+`3PA` +`3P%` + FTA + `FT%` + REB + TOV+ STL), data=nba)
 summary(wreg)
 
 #Análisis de supuestos
 #ANNOVA
+anovawreg<-aov(wreg)
 summary(aov(wreg))
 boxplot(wreg$residuals)
+plot(anovawreg)
 
 #colinealidad
 pairs(exploranba)
